0e0d8d2acc4f0bccfd19ef7b2f72df04aa4014f1
Mikel-Jorge <mjorgesote@educacion.navarra.es>
2025-10-08 10:09:22 +0200

    Ultimos cambios

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,5 +18,10 @@ public class GameManager : MonoBehaviour
     {
         monedas++;
         Debug.Log("Monedas: " + monedas);
+    }
+
+    public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
     }
 }
